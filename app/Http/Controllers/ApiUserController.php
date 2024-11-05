54dfa660bb2ae89a093f89ba42777633ed74cd4b
Aleksei-chance <aleksei.chance@gmail.com>
2024-11-05 17:04:27 +0300

    ApiUserController

---
+++ app/Http/Controllers/ApiUserController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ApiUserController extends Controller
+{
+    public function connect(){
+        return 'yes';
+    }
+}
