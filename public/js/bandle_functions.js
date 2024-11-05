12aca94eb897a3af630aee3a84e9c95fbe46aac6
Aleksei-chance <aleksei.chance@gmail.com>
2024-11-06 02:18:15 +0300

    bandle_actions

--- public/js/bandle_functions.js
+++ public/js/bandle_functions.js
@@ -1,15 +1,50 @@
 var TOKEN = $('meta[name="csrf-token"]').attr('content');
 
-function user_page_load(type) {
+function user_page_load(type)
+{
     $.ajax({
         url: "/logic/user",
         method: "post",
         dataType: "html",
         data: {_token: TOKEN, func: 'page_load', type: type}
-    }).done(function(data){
+    }).done(function(data)
+    {
         $("#content").html(data);
-        // bandle_actions(type_view);
-    }).fail(function(data){
+        bandle_actions(0);
+    }).fail(function(data)
+    {
         
+    });
+
+    
+}
+
+function bandle_actions(type_view)
+{
+    let check = 0;
+
+    $(".bundle_action").bind("touchstart mousedown", function(e)
+    {
+        let id = $(e.currentTarget).attr('id').replaceAll('bandle_', '');
+        check = 1;
+        Timer = setTimeout(function() 
+        {
+            if(type_view == 0)
+            {
+                bandle_renew_item(id);
+            }
+            check = 0;
+        }, 300);
+    });
+
+
+    $(".bundle_action").bind("touchend mouseup", function(e)
+    {
+        let id = $(e.currentTarget).attr('id').replaceAll('bandle_', '');
+        if(check)
+        {
+            location.href = "/bandle/"+id;
+        }
+        clearTimeout(Timer);
     });
 }
