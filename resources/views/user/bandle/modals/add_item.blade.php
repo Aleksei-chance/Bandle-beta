01948010051b56c72de91ebac973559ca156f7bf
Aleksei-chance <aleksei.chance@gmail.com>
2024-11-07 01:09:43 +0300

    component Input

--- resources/views/user/bandle/modals/add_item.blade.php
+++ resources/views/user/bandle/modals/add_item.blade.php
@@ -5,13 +5,8 @@
 @section('title', 'New Bande')
 
 @section('content')
-<div class="input_block" id="title_block">
-    <input type="text" class="input_simple text_black" placeholder="Title" id="title"  oninput="input_valid(this)">
-    <p class="error_text"></p>
-</div>
-<div class="input_block" id="description_block">
-    <input type="text" class="input_simple text_black" placeholder="Description" id="description"  oninput="input_valid(this)">
-</div>
+<x-Input id="title" placeholder="Title" oninput="input_valid(this)"/>
+<x-Input id="description" placeholder="Description" oninput="input_valid(this)"/>
 @endsection
 
 @section('button')
