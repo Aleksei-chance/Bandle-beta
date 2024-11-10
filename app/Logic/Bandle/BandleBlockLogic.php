db95d2ea135601bd991d197aa222d587977984d7
Aleksei-chance <aleksei.chance@gmail.com>
2024-11-10 23:44:21 +0300

    bandle_block_item_add_modal content

--- app/Logic/Bandle/BandleBlockLogic.php
+++ app/Logic/Bandle/BandleBlockLogic.php
@@ -9,8 +9,20 @@ class BandleBlockLogic
 {
     public static function item_add_modal($bandle_id)
     {
-        $arr = array();
-    
+        $items = array();
+        $block_types = BlockType::query()->orderBy('sort', 'asc')->get();
+        foreach($block_types as $item)
+        {
+            $limit = $item->limit;
+            $count = Bandle::query()->find($bandle_id)->blocks_count($item->id);
+            if($count < $limit || $limit == 0) {
+                $items[] = $item->toArray();
+            }
+        }
+        $arr = array(
+            'id' => $bandle_id
+            , 'items' => $items
+        );
         return view('bandle.block.modals.item_add', $arr);
     } 
 }
--- resources/views/bandle/block/modals/item_add.blade.php
+++ resources/views/bandle/block/modals/item_add.blade.php
@@ -8,5 +8,13 @@
 @endsection
 
 @section('content')
-
+@foreach ($items as $item)
+<div class="block_add_item_block" onclick="bandle_block_item_add_send({{ $id }}, {{ $item['id'] }})">
+    <i class="{{ $item["icon"] }}"></i>
+    <div class="block_add_item_text">
+        <p class="block_add_item_name">{{ $item["name"] }}</p>
+        <p class="block_add_item_description">{{ $item["description"] }}</p>
+    </div>
+</div>
+@endforeach
 @endsection
